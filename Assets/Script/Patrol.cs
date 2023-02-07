a77e53cddd953ea8d76162c2c345e086bc2ad5ba
SonicLee0507 <sam920507@gmail.com>
2023-02-08 01:38:44 +0800

    Patrol

--- Assets/Script/Patrol.cs
+++ Assets/Script/Patrol.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
+    public Transform[] patrolPoints;
+    public float speed;
+    int currentPointIndex;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        transform.position = patrolPoints[0].position;
     }
 
     // Update is called once per frame
-    void Update()
+   private void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
+
+        if (transform.position == patrolPoints[currentPointIndex].position)
+        {
+            if (currentPointIndex + 1 < patrolPoints.Length)
+            {
+                currentPointIndex++;
+            }
+            else
+            {
+                currentPointIndex = 0;
+            }
+        }
     }
 }
